32b12395143a6b693b0b0a298864350a793e25be
Atul Sharma <atulsharma@Atuls-MacBook-Air.local>
2026-06-26 00:30:07 +0530

    Restore skills icons: fix skills.json paths, quote img src in script, remove hardcoded tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,15 +92,15 @@ async function fetchData(type = "skills") {
 function showSkills(skills) {
     let skillsContainer = document.getElementById("skillsContainer");
     let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
+        skills.forEach(skill => {
+                skillHTML += `
+                <div class="bar">
+                            <div class="info">
+                                <img src="${skill.icon}" alt="skill" />
+                                <span>${skill.name}</span>
+                            </div>
+                        </div>`
+        });
     skillsContainer.innerHTML = skillHTML;
 }
 
